9669047a2a574b2b6106fb4da578ecfaf962d56e
Mounir Raki <mouniraki.me@gmail.com>
2020-03-01 20:58:16 +0100

    Adding comments to the class files, tests soon to be written

--- src/ch/epfl/rigel/astronomy/Epoch.java
+++ src/ch/epfl/rigel/astronomy/Epoch.java
@@ -3,6 +3,12 @@ package ch.epfl.rigel.astronomy;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * An astronomical epoch.
+ *
+ * @author Mounir Raki
+ */
+
 public enum Epoch {
     J2000(ZonedDateTime.of(
             LocalDate.of(2000, Month.JANUARY, 1),
@@ -19,16 +25,31 @@ public enum Epoch {
     private static final double MS_PER_DAY = 1000 * 60 * 60 * 24;
     private static final double MS_PER_CENTURY = MS_PER_DAY * 365.25 * 100;
 
-    private Epoch(ZonedDateTime epochDate){
+    Epoch(ZonedDateTime epochDate){
         date = epochDate;
     }
 
+    /**
+     * Calculates the number of days between an epoch and an instant.
+     *
+     * @param when
+     *          an instant (in ZonedDateTime)
+     *
+     * @return the number of days between the epoch and the instant
+     */
     public double daysUntil(ZonedDateTime when){
        double msSplit = date.until(when, ChronoUnit.MILLIS);
        return msSplit/MS_PER_DAY;
     }
 
-    //A REVOIR
+    /**
+     * Calculates the number of julian centuries between an epoch and an instant.
+     *
+     * @param when
+     *          an instant (in ZonedDateTime)
+     *
+     * @return the number of julian centuries between the epoch and the instant
+     */
     public double julianCenturiesUntil(ZonedDateTime when){
         double msSplit = date.until(when, ChronoUnit.MILLIS);
         return msSplit/MS_PER_CENTURY;
--- src/ch/epfl/rigel/astronomy/SiderealTime.java
+++ src/ch/epfl/rigel/astronomy/SiderealTime.java
@@ -8,9 +8,22 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * A sidereal time.
+ *
+ * @author Mounir Raki
+ */
 public final class SiderealTime {
     private SiderealTime(){}
 
+    /**
+     * Calculates the greenwich sidereal time from a couple date/time in radians.
+     *
+     * @param when
+     *          a couple date/time (in ZonedDateTime)
+     *
+     * @return the greenwich sidereal time, in radians and normalized to the interval [0, TAU[
+     */
     public static double greenwich(ZonedDateTime when){
         ZonedDateTime whenInUTC = when.withZoneSameInstant(ZoneOffset.UTC);
         double julianCenturies = Epoch.J2000.julianCenturiesUntil(whenInUTC.truncatedTo(ChronoUnit.DAYS));
@@ -24,6 +37,17 @@ public final class SiderealTime {
         return Angle.normalizePositive(Angle.ofHr(siderealGreenwichInHours));
     }
 
+    /**
+     * Calculates the local sidereal time in radians, from a couple date/time
+     * and a geographic coordinate of the location.
+     *
+     * @param when
+     *          a couple date/time (in ZonedDateTime)
+     * @param where
+     *          the coordinates of the location from where to find the sidereal time (in GeographicCoordinates)
+     *
+     * @return the local sidereal time, in radians and normalized to the interval [0, TAU[
+     */
     public static double local(ZonedDateTime when, GeographicCoordinates where){
         double siderealLocal = greenwich(when)+where.lon();
         return Angle.normalizePositive(siderealLocal);
