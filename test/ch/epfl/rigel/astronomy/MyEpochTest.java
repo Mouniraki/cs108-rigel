d92bb652477dfb9d06fb0ae1c3dceb0238eff3ce
mouniraki <mouniraki.me@gmail.com>
2020-03-02 13:20:58 +0100

    Modifying test files for Epoch and SiderealTime, more tests to be done

--- test/ch/epfl/rigel/astronomy/MyEpochTest.java
+++ test/ch/epfl/rigel/astronomy/MyEpochTest.java
@@ -24,9 +24,29 @@ class MyEpochTest {
                 LocalTime.of(12, 35, 59),
                 ZoneOffset.UTC);
 
+        var march3_2020 = ZonedDateTime.of(
+                LocalDate.of(2020, Month.MARCH, 3),
+                LocalTime.of(23, 48, 32),
+                ZoneOffset.UTC);
+
+        var jan1_2000 = ZonedDateTime.of(
+                LocalDate.of(2000, Month.JANUARY, 1),
+                LocalTime.of(12, 0, 0),
+                ZoneOffset.UTC);
+
+        var dec31_2009 = ZonedDateTime.of(
+                LocalDate.of(2009, Month.DECEMBER, 31),
+                LocalTime.of(0, 0, 0),
+                ZoneOffset.UTC);
+
         assertEquals(2.25, Epoch.J2000.daysUntil(jan3_2000));
         assertEquals(7365.1435416667, Epoch.J2000.daysUntil(march1_2020), 1e-10);
         assertEquals(-191.475011574, Epoch.J2010.daysUntil(june22_2009), 1e-10);
+        assertEquals(3715.992037037, Epoch.J2010.daysUntil(march3_2020), 1e-10);
+
+        assertEquals(0, Epoch.J2000.daysUntil(jan1_2000));
+        assertEquals(0, Epoch.J2010.daysUntil(dec31_2009));
+
     }
 
     void julianCenturiesUntilTest(){
