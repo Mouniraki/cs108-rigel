dabf6443182b31637e0d4b31cbe3eb3c6031a4db
mouniraki <mouniraki.me@gmail.com>
2020-03-10 23:04:07 +0100

    Completing test classes, must be sure that we have to normalize the lambda value in the inverseApply method from StereographicProjection, because sometimes it outputs values higher than TAU which is illegal

--- test/ch/epfl/rigel/coordinates/MyCartesianCoordinatesTest.java
+++ test/ch/epfl/rigel/coordinates/MyCartesianCoordinatesTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.coordinates;
 
+import ch.epfl.test.TestRandomizer;
 import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
@@ -9,9 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 class MyCartesianCoordinatesTest {
     @Test
     void ofWorksWithAllValues(){
+        var rng = TestRandomizer.newRandom();
         for(int i=0; i<1000; ++i){
-            double x = Math.random()*1000;
-            double y = Math.random()*i;
+            var x = rng.nextDouble(-1000000, 1000000);
+            var y = rng.nextDouble(-1000000, 1000000);
             var c = CartesianCoordinates.of(x, y);
             assertEquals(x, c.x());
             assertEquals(y, c.y());
@@ -20,9 +22,10 @@ class MyCartesianCoordinatesTest {
 
     @Test
     void toStringWorksProperly(){
+        var rng = TestRandomizer.newRandom();
         for(int i=0; i<1000; ++i){
-            double x = Math.random()*1000;
-            double y = Math.random()*i;
+            var x = rng.nextDouble(-1000000, 1000000);
+            var y = rng.nextDouble(-1000000, 1000000);
             var c = CartesianCoordinates.of(x, y);
             var s = String.format(Locale.ROOT, "Cartesian coordinates: (x=%.4f, y=%.4f)", x, y);
             assertEquals(s, c.toString());
