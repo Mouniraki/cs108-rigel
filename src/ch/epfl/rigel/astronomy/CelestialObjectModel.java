7532a3f6892b78c87ed83bea827f726866f824ab
mouniraki <mouniraki.me@gmail.com>
2020-03-27 14:32:39 +0100

    Changed a bit the comments in CelestialObjectModel

--- src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
+++ src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
@@ -11,9 +11,12 @@ import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
  */
 public interface CelestialObjectModel<O> {
     /**
-     * Abstract method. Models the number (can be negative) of days after given epoch J2010.
-     * @param daysSinceJ2010 Number of days after the epoch J2010
-     * @param eclipticToEquatorialConversion Conversion from ecliptic to equatorial coordinates
+     * Abstract method. Models a new CelestialObject for the number (can be negative) of days after given epoch J2010.
+     *
+     * @param daysSinceJ2010
+     *          Number of days after the epoch J2010
+     * @param eclipticToEquatorialConversion
+     *          Conversion from ecliptic to equatorial coordinates
      * @return Modelised object by the model for the number of days after epoch J2010
      */
     public abstract O at(double daysSinceJ2010, EclipticToEquatorialConversion eclipticToEquatorialConversion);
