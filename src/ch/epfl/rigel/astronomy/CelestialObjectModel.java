289628dcd81c2ebb5689c3dbe8de9a42b0b85e56
mouniraki <mouniraki.me@gmail.com>
2020-03-27 09:28:40 +0100

    Commenting the public entities of Star and Asterism, changed the assignation method to have an immutable list, tests to be finalized

--- src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
+++ src/ch/epfl/rigel/astronomy/CelestialObjectModel.java
@@ -11,7 +11,7 @@ import ch.epfl.rigel.coordinates.EclipticToEquatorialConversion;
  */
 public interface CelestialObjectModel<O> {
     /**
-     * Abstract method. Modelises the number (can be negative) of days after given epoch J2010.
+     * Abstract method. Models the number (can be negative) of days after given epoch J2010.
      * @param daysSinceJ2010 Number of days after the epoch J2010
      * @param eclipticToEquatorialConversion Conversion from ecliptic to equatorial coordinates
      * @return Modelised object by the model for the number of days after epoch J2010
