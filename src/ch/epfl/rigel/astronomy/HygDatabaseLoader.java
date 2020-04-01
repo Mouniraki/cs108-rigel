df25641e5d56b72586878f3f175194bf39985e6f
mouniraki <mouniraki.me@gmail.com>
2020-04-01 23:39:17 +0200

    Implemented HygDatabaseLoader, tests must be done

--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -1,8 +1,8 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
+
 import java.io.*;
-import java.util.Arrays;
-import java.util.List;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
@@ -14,17 +14,47 @@ public enum HygDatabaseLoader implements StarCatalogue.Loader{
     public void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException {
         try(BufferedReader r = new BufferedReader(new InputStreamReader(inputStream, US_ASCII))){
             String line;
+            int i=0;
             while((line = r.readLine()) != null){
                String[] strArray = line.split(",");
-                for (int i = 0; i < strArray.length; ++i) {
-                    StarCatalogue.Builder
-                }
-               System.out.println(Arrays.toString(strArray));
+               if(i > 0) {
+                   int hipparcosId = 0;
+                   String proper;
+                   double ra = Double.parseDouble(strArray[colIndexes.RARAD.ordinal()]);
+                   double dec = Double.parseDouble(strArray[colIndexes.DECRAD.ordinal()]);
+                   double magnitude = 0;
+                   double colorIndex = 0;
+                   String bayer = "?";
+                   String con = strArray[colIndexes.CON.ordinal()];
+
+                   if (!strArray[colIndexes.HIP.ordinal()].isEmpty())
+                       hipparcosId = Integer.parseInt(strArray[colIndexes.HIP.ordinal()]);
+
+                   if (!strArray[colIndexes.BAYER.ordinal()].isEmpty())
+                       bayer = strArray[colIndexes.BAYER.ordinal()];
+
+                   if (!strArray[colIndexes.PROPER.ordinal()].isEmpty())
+                       proper = strArray[colIndexes.PROPER.ordinal()];
+
+                   else
+                       proper = bayer + " " + con;
+
+                   if (!strArray[colIndexes.MAG.ordinal()].isEmpty())
+                       magnitude = Double.parseDouble(strArray[colIndexes.MAG.ordinal()]);
+
+                   if (!strArray[colIndexes.CI.ordinal()].isEmpty())
+                       colorIndex = Double.parseDouble(strArray[colIndexes.CI.ordinal()]);
+
+                   EquatorialCoordinates equatorialPos = EquatorialCoordinates.of(ra, dec);
+
+                   builder.addStar(new Star(hipparcosId, proper, equatorialPos, (float) magnitude, (float) colorIndex));
+               }
+               ++i;
             }
         }
     }
 
-    private enum columnIndexes{
+    private enum colIndexes{
         ID, HIP, HD, HR, GL, BF, PROPER, RA, DEC, DIST, PMRA, PMDEC,
                 RV, MAG, ABSMAG, SPECT, CI, X, Y, Z, VX, VY, VZ,
                 RARAD, DECRAD, PMRARAD, PMDECRAD, BAYER, FLAM, CON,
--- test/ch/epfl/rigel/astronomy/MyHygDatabaseLoaderTest.java
+++ test/ch/epfl/rigel/astronomy/MyHygDatabaseLoaderTest.java
@@ -1,5 +1,6 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -26,6 +27,13 @@ class MyHygDatabaseLoaderTest {
             StarCatalogue catalogue = new StarCatalogue.Builder()
                     .loadFrom(hygStream, HygDatabaseLoader.INSTANCE)
                     .build();
+            /*
+            for(Star s : catalogue.stars()){
+                String star = s.hipparcosId() + " " + s.name() + " " + s.equatorialPos() + " " + s.magnitude() + " " + s.colorTemperature();
+                System.out.println(star);
+            }
+
+             */
             Star rigel = null;
             for (Star s : catalogue.stars()) {
                 if (s.name().equalsIgnoreCase("rigel"))
