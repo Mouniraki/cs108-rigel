5933366616344607363a1d93b8f20051f7f228ab
nicolas <nicolas.szwajcok@gmail.com>
2020-04-02 00:36:13 +0200

    StarCatalogue should be finished, need to test it

--- src/ch/epfl/rigel/astronomy/StarCatalogue.java
+++ src/ch/epfl/rigel/astronomy/StarCatalogue.java
@@ -1,88 +1,63 @@
 package ch.epfl.rigel.astronomy;
 
+import ch.epfl.rigel.Preconditions;
+
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public final class StarCatalogue {
     final private List<Star> stars;
     final private List<Asterism> asterisms;
-    private Map<Integer, Integer> map = new HashMap<>();
+    private final Map<Asterism, List<Integer>> map = new HashMap<>();
 
+    public StarCatalogue(List<Star> stars, List<Asterism> asterisms) {
+        this.stars = List.copyOf(stars);
+        this.asterisms = List.copyOf(asterisms);
 
-    public StarCatalogue(List<Star> stars, List<Asterism> asterisms){
-        for (Asterism asterism : asterisms) {
-//            if(!stars.contains(asterism)){
-//                throw new IllegalArgumentException("One of the asterisms of this star catalogue is not contained in the list of the stars.");
-//            }
+        for (int a = 0; a < asterisms.size(); ++a){
+            for(int b = 0; b < asterisms.get(a).stars().size(); ++b){
+                if(!stars.contains(asterisms.get(a).stars().get(b))){
+                    throw new IllegalArgumentException("One of the asterisms of this star catalogue is not contained in the list of the stars.");
+                }
+            }
         }
 
-        InputStream is = this.getClass().getResourceAsStream("/asterisms.txt");
-        Reader isReader = new InputStreamReader(is, StandardCharsets.UTF_8);
-
-
-        String toAppend = "";
-        int asterismIndex = 0;
-        StringBuffer sb = new StringBuffer();
-        String str = "";
-        BufferedReader reader = new BufferedReader(isReader);
-        System.out.println("SHDHSHHDHDSHDSNJDSN");
-
-
-//        while((str = reader.readLine())!= null){
-//            sb.append(str);
-//        }
-//        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("txtPath"),"ISO-8859-1"));
-//        for(int i = 0; i < 15; ++i){
+        for(Asterism a : asterisms){
+            List<Integer> indexes = new ArrayList<>();
+            for(Star s : stars){
+                if(a.stars().contains(s))
+                    indexes.add(stars.indexOf(s));
+            }
+            map.put(a, List.copyOf(indexes));
+        }
+    }
+//        InputStream is = this.getClass().getResourceAsStream("/asterisms.txt");
+//        Reader isReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+//        int asterismIndex = 0;
+//        String str = "";
+//        BufferedReader reader = new BufferedReader(isReader);
+//        Character readerObj;
+//        boolean loopControler = true;
+//        while (loopControler) {
 //            try {
-//                Character readerObj = (char) reader.read();
-//                if(readerObj == ',') {
+//                readerObj = (char) reader.read();
+//                if (readerObj == Character.MAX_VALUE) {
+//                    loopControler = false;
+//                }
+//
+//                if (readerObj == ',' || readerObj == '\n') {
 //                    map.put(Integer.parseInt(str), asterismIndex);
+//                    System.out.println(Integer.parseInt(str) + " : " + asterismIndex);
 //                    str = "";
 //                    asterismIndex += 1;
+//                } else {
+//                    str += readerObj;
 //                }
-//                else{
-//                    str.concat(readerObj.toString());
-//                }
-//                System.out.println(str);
-//
 //            } catch (IOException e) {
 //                e.printStackTrace();
 //            }
-//            System.out.println(str);
-
-//            for(int in = 0; in < map.size(); ++in){
-//                System.out.println(map.get(in));
-//            }
-//            if(){
-//
-//            }
-//            if(reader == ","){
-//                asterismIndex += 1;
-//            }
-//            int value = 10;
-//            value += reader;
-//            toAppend.concat(isReader.read());
-//            int data = isReader.read();
-
-//            if(isReader.read() == -1){
-//
-//            }
-
-//            map.put(isReader, );
-//            map.put(asterisms.get(i), isReader.read());
 //        }
 
-//        for(int i = 0; i < asterisms.size(); ++i){
-//            map.put(asterisms.get(i), );
-////            map.put(asterisms.get(i), isReader.read());
-//            isReader.
-//        }
-
-
-        this.stars = stars;
-        this.asterisms = asterisms;
-    }
 
     public List<Star> stars(){
         return stars;
@@ -93,38 +68,10 @@ public final class StarCatalogue {
     }
 //output index in the catalogue
     public List<Integer> asterismIndices(Asterism asterism){//in test check the lengths of two arrays
-        List<Star> asterismStars = asterism.stars();
-        List<Integer> indices = new ArrayList<>();
-
-
-
-//        System.out.println(is);
-//        System.out.println(is.readNBytes(10));
-        return null;
-// output
-//        InputStream imkdds = new FileInputStream("/resources/asterisms.txt");
-//
-//        InputStream imkfdsdds = new FileInputStream("resources//asterisms.txt");
-//
-//        InputStream i = new FileInputStream("/home/nicolas/IdeaProjects/Rigel/resources/asterisms.txt");
-////        InputStream iss =  ObjectInputFilter.Config.class.getResourceAsStream("dfsfdfsdf");
-//
-////        InputStream i = new FileInputStream("./home/nicolas/IdeaProjects/Rigel/resources/asterisms.txt");
-////        Reader i = new InputStreamReader(
-////                new FileInputStream("resources/asterisms.txt"));
-////        File file = new File("C:/Java2blog.txt");
-//        File file = new File("resources/asterisms.txt");
-//        Reader io = new InputStreamReader(
-//                new FileInputStream("resources/asterisms.txt"),
-//                StandardCharsets.UTF_8);
-//        Reader iof = new InputStreamReader(
-//                new FileInputStream("resources/asterisms.txt"),
-//                StandardCharsets.UTF_8);
-
-
-
+        Preconditions.checkArgument(asterisms.contains(asterism));
+        List<Integer> indices = map.get(asterism);
+        return indices;
     }
-
 
     public final static class Builder{
         private List<Star> stars;
@@ -153,16 +100,14 @@ public final class StarCatalogue {
             return Collections.unmodifiableList(asterisms);
         }
 
-        //TODO class
         public Builder loadFrom(InputStream inputStream, Loader loader) throws IOException {
-
-            return null;
+            loader.load(inputStream, this);
+            return this;
         }
 
         public StarCatalogue build(){
             return new StarCatalogue(stars, asterisms);
         }
-
     }
 
     public interface Loader {
