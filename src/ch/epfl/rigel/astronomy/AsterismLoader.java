c30acc01b997c133d30fe97ad4a77403f78df402
mouniraki <mouniraki.me@gmail.com>
2020-04-02 22:54:06 +0200

    Modified slightly the HygDatabaseLoader and AsterismLoader to try to optimize them, added some comments in these classes, continuing the test procedures

--- src/ch/epfl/rigel/astronomy/AsterismLoader.java
+++ src/ch/epfl/rigel/astronomy/AsterismLoader.java
@@ -9,30 +9,43 @@ import java.util.List;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
+/**
+ * Loader for Asterisms.
+ *
+ * @author Mounir Raki (310287)
+ */
 public enum AsterismLoader implements StarCatalogue.Loader{
     INSTANCE;
 
+    /**
+     * Loads a file containing the information of the asterisms from a text file,
+     * and builds the list of asterisms in a StarCatalogue.
+     *
+     * @param inputStream
+     *          the content of the file to read, obtained by an InputStream
+     * @param builder
+     *          the type of builder, used to build the list of asterisms
+     * @throws IOException
+     *          if the file doesn't exist or the path is incorrect
+     */
     @Override
     public void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException {
         try(BufferedReader r = new BufferedReader(new InputStreamReader(inputStream, US_ASCII))){
             String line;
-            int i=0;
-            while((line = r.readLine()) != null){
+            while((line = r.readLine()) != null) {
                 String[] strArray = line.split(",");
                 List<Star> stars = new ArrayList<>();
-                for(String hipId : strArray){
+
+                for(String hipId : strArray) {
                     int hipparcosId = Integer.parseInt(hipId);
                     for(Star star : builder.stars()){
-                        if(star.hipparcosId() == hipparcosId) {
+                        if(star.hipparcosId() == hipparcosId)
                             stars.add(star);
-                            ++i;
-                        }
                     }
                 }
 
                 builder.addAsterism(new Asterism(stars));
             }
-            System.out.println(i);
         }
     }
 }
--- src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
+++ src/ch/epfl/rigel/astronomy/HygDatabaseLoader.java
@@ -5,51 +5,49 @@ import ch.epfl.rigel.coordinates.EquatorialCoordinates;
 import java.io.*;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
-
+/**
+ * Loader for HYG Database.
+ *
+ * @author Mounir Raki (310287)
+ */
 public enum HygDatabaseLoader implements StarCatalogue.Loader{
     INSTANCE;
 
-
+    /**
+     * Loads a file containing the information of the stars from a HYG catalog,
+     * and builds the list of stars in a StarCatalogue.
+     *
+     * @param inputStream
+     *          the content of the file to read, obtained by an InputStream
+     * @param builder
+     *          the type of builder, used to build the list of stars
+     * @throws IOException
+     *          if the file doesn't exist or the path is incorrect
+     */
     @Override
     public void load(InputStream inputStream, StarCatalogue.Builder builder) throws IOException {
         try(BufferedReader r = new BufferedReader(new InputStreamReader(inputStream, US_ASCII))){
+            r.readLine();
             String line;
-            int i=0;
             while((line = r.readLine()) != null) {
-               String[] strArray = line.split(",");
-               if(i > 0) {
-                   int hipparcosId = 0;
-                   String proper;
-                   double ra = Double.parseDouble(strArray[ColName.RARAD.ordinal()]);
-                   double dec = Double.parseDouble(strArray[ColName.DECRAD.ordinal()]);
-                   double magnitude = 0;
-                   double colorIndex = 0;
-                   String bayer = "?";
-                   String con = strArray[ColName.CON.ordinal()];
+                String[] strArray = line.split(",");
 
-                   if (!strArray[ColName.HIP.ordinal()].isEmpty())
-                       hipparcosId = Integer.parseInt(strArray[ColName.HIP.ordinal()]);
+                double ra = Double.parseDouble(strArray[ColName.RARAD.ordinal()]);
+                double dec = Double.parseDouble(strArray[ColName.DECRAD.ordinal()]);
+                String con = strArray[ColName.CON.ordinal()];
+                EquatorialCoordinates equatorialPos = EquatorialCoordinates.of(ra, dec);
 
-                   if (!strArray[ColName.BAYER.ordinal()].isEmpty())
-                       bayer = strArray[ColName.BAYER.ordinal()];
+                int hipparcosId = !(strArray[ColName.HIP.ordinal()].isEmpty()) ? Integer.parseInt(strArray[ColName.HIP.ordinal()]) : 0;
 
-                   if (!strArray[ColName.PROPER.ordinal()].isEmpty())
-                       proper = strArray[ColName.PROPER.ordinal()];
+                double magnitude = !(strArray[ColName.MAG.ordinal()].isEmpty()) ? Double.parseDouble(strArray[ColName.MAG.ordinal()]) : 0;
 
-                   else
-                       proper = bayer + " " + con;
+                double colorIndex = !(strArray[ColName.CI.ordinal()].isEmpty()) ? Double.parseDouble(strArray[ColName.CI.ordinal()]) : 0;
 
-                   if (!strArray[ColName.MAG.ordinal()].isEmpty())
-                       magnitude = Double.parseDouble(strArray[ColName.MAG.ordinal()]);
+                String bayer = !(strArray[ColName.BAYER.ordinal()].isEmpty()) ? strArray[ColName.BAYER.ordinal()] : "?";
 
-                   if (!strArray[ColName.CI.ordinal()].isEmpty())
-                       colorIndex = Double.parseDouble(strArray[ColName.CI.ordinal()]);
+                String proper = !(strArray[ColName.PROPER.ordinal()].isEmpty()) ? strArray[ColName.PROPER.ordinal()] : bayer + " " + con;
 
-                   EquatorialCoordinates equatorialPos = EquatorialCoordinates.of(ra, dec);
-
-                   builder.addStar(new Star(hipparcosId, proper, equatorialPos, (float) magnitude, (float) colorIndex));
-               }
-               ++i;
+                builder.addStar(new Star(hipparcosId, proper, equatorialPos, (float) magnitude, (float) colorIndex));
             }
         }
     }
