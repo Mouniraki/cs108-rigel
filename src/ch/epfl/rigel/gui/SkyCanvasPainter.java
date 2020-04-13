d04f7c8275de9bb3e8435971774c5e0e7227cbc3
nicolas <nicolas.szwajcok@gmail.com>
2020-04-14 01:15:50 +0200

    Latest version of the SkyCanvasPainter, not yet finished

--- src/ch/epfl/rigel/gui/SkyCanvasPainter.java
+++ src/ch/epfl/rigel/gui/SkyCanvasPainter.java
@@ -8,10 +8,14 @@ import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineJoin;
 import javafx.scene.transform.Scale;
 import javafx.scene.transform.Transform;
 
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class SkyCanvasPainter {
     private final ClosedInterval interval;
@@ -45,8 +49,57 @@ public class SkyCanvasPainter {
     }
 
     public void drawStars(ObservedSky sky, StereographicProjection projection, Transform transform){
-        List<Star> stars = sky.stars();
+        Set<Asterism> asterismsList = sky.asterisms();
+        Iterator<Asterism> asterismIterator = asterismsList.iterator();
         EquatorialToHorizontalConversion conversion = new EquatorialToHorizontalConversion(sky.observationInstant(), sky.observationPos());
+
+        while(asterismIterator.hasNext()){
+            Asterism asterism = asterismIterator.next();
+            List<Star> stars = asterism.stars();
+            ctx.setLineWidth(1);
+            ctx.setStroke(Color.BLUE);
+            ctx.setLineJoin(StrokeLineJoin.ROUND);
+
+            ctx.beginPath();
+
+            for (Star star : stars){
+                HorizontalCoordinates coordinates = conversion.apply(star.equatorialPos());
+                CartesianCoordinates projCoord = projection.apply(coordinates);
+
+                Point2D point2D = transform.transform(projCoord.x(), projCoord.y());
+
+                ctx.moveTo(point2D.getX(), point2D.getY());//TODO add if for only visible values
+                ctx.lineTo(point2D.getX(), point2D.getY());
+            }
+            ctx.stroke();
+            ctx.closePath();
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        List<Star> stars = sky.stars();
         //BlackBodyColor.readFile(); //NO NEED FOR INITIALIZATION OF THE FILE, YOU JUST NEED TO CALL THE COLORTEMPERATURE METHOD NOW
 
         for(Star star : stars){
@@ -58,6 +111,7 @@ public class SkyCanvasPainter {
 
             double sizeFactor = (99 - (17 * starSize)) / 140;
             double diameter = sizeFactor * 2 * Math.tan( (Angle.ofDeg(0.5)) / 4 );
+            System.out.println(diameter);
 
             Color color = BlackBodyColor.colorForTemperature(star.colorTemperature());
 
@@ -77,24 +131,15 @@ public class SkyCanvasPainter {
         CartesianCoordinates projCoord = projection.apply(coordinates);
         Point2D point2D = transform.transform(projCoord.x(), projCoord.y());
 
-//        double sunSize = sun.angularSize();
-        double diameter = 2 * Math.tan( (Angle.ofDeg(0.5)) / 4 );
-        ctx.setFill(Color.YELLOW);
+        double diameter = 2 * Math.tan( (Angle.ofDeg(0.5)) / 4 ); //should the diameter be this constant or I should calculate it?
+        ctx.setFill(Color.YELLOW);//TODO deriveHalo
 
-        if(diameter * 2.2 > diameter + 2){
-            ctx.setGlobalAlpha(0.25);
-            ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx()*2.2, diameter*transform.getTy()*2.2 );
-            ctx.setGlobalAlpha(1.0);
+        ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx()+2, diameter*transform.getTy()+2 );
 
-            ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx()+2, diameter*transform.getTy()+2 );
-        }
-        else{
-            ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx()+2, diameter*transform.getTy()+2 );
+        ctx.setGlobalAlpha(0.25);
+        ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx()*2.2, diameter*transform.getTy()*2.2 );
+        ctx.setGlobalAlpha(1.0);
 
-            ctx.setGlobalAlpha(0.25);
-            ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx()*2.2, diameter*transform.getTy()*2.2 );
-            ctx.setGlobalAlpha(1.0);
-        }
 
         ctx.setFill(Color.WHITE);
         ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx(), diameter*transform.getTy() );
@@ -117,15 +162,13 @@ public class SkyCanvasPainter {
             ctx.setFill(Color.LIGHTGRAY);
             ctx.fillOval(point2D.getX(), point2D.getY(), diameter*transform.getTx(), diameter*transform.getTy()); //not sure if correct diameter, but very similar to Professor's sky.png file
         }
-
     }
 
     public void drawSkyCanvas(ObservedSky sky, StereographicProjection projection, Transform transform){
         clear();
         drawStars(sky, projection, transform);
+        drawPlanets(sky, projection, transform);
         drawSun(sky, projection, transform);
         drawMoon(sky, projection, transform);//and so on, order, 1étoiles, 2planètes, 3Soleil, 4Lune.
-
     }
-
 }
